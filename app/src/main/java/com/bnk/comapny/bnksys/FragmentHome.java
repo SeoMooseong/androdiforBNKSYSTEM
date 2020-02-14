1fc38b014866ffb9ea4677bb82f87874bda9d547
ryuhojin <rlawgns077@naver.com>
2020-02-14 17:51:35 +0900

    DESIGN : CHART and MAIN

--- app/src/main/java/com/bnk/comapny/bnksys/FragmentHome.java
+++ app/src/main/java/com/bnk/comapny/bnksys/FragmentHome.java
@@ -1,5 +1,6 @@
 package com.bnk.comapny.bnksys;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,11 +9,64 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FragmentHome extends Fragment {
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.util.ArrayList;
+
+public class FragmentHome extends Fragment {
+    PieChart pieChart;
+    View homeview;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home,container,false);
+
+          homeview = inflater.inflate(R.layout.home,container,false);
+          chartMoney();
+          return homeview;
+    }
+
+    public void chartMoney(){
+        pieChart = (PieChart)homeview.findViewById(R.id.money);
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10,5,5);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(60f);
+
+        ArrayList<PieEntry> yValues = new ArrayList<PieEntry>();
+
+        yValues.add(new PieEntry(80f,"월급"));
+        yValues.add(new PieEntry(20f,"대출"));
+
+        pieChart.setCenterText("월급 대출 비");
+        pieChart.setCenterTextSize(14f);
+        pieChart.setCenterTextColor(Color.rgb(122, 122, 122));
+
+        pieChart.animateY(1000, Easing.EaseInOutCubic); //애니메이션
+        int[] color_pie = {Color.rgb(255, 118, 113),Color.rgb(255, 179, 113)};
+        PieDataSet dataSet = new PieDataSet(yValues,"");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(color_pie);
+
+        PieData data = new PieData((dataSet));
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setData(data);
+
     }
 }
--- app/src/main/java/com/bnk/comapny/bnksys/MainActivity.java
+++ app/src/main/java/com/bnk/comapny/bnksys/MainActivity.java
@@ -1,12 +1,20 @@
 package com.bnk.comapny.bnksys;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,27 +29,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setElevation(0);
         FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.frameLayout,home).commitAllowingStateLoss();
+        transaction.replace(R.id.frameLayout, home).commitAllowingStateLoss();
 
         BottomNavigationView bnaviview = findViewById(R.id.navigationView);
 
         bnaviview.setOnNavigationItemSelectedListener(new ItemSelectedListener());
     }
-    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_menu, menu);
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setQueryHint("아파트 명을 입력해주세요");
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             FragmentTransaction transaction = manager.beginTransaction();
 
-            switch(menuItem.getItemId())
-            {
+            switch (menuItem.getItemId()) {
                 case R.id.home:
-                    transaction.replace(R.id.frameLayout,home).commitAllowingStateLoss();
+                    transaction.replace(R.id.frameLayout, home).commitAllowingStateLoss();
                     break;
                 case R.id.analysis:
-                    transaction.replace(R.id.frameLayout,analysis).commitAllowingStateLoss();
+                    transaction.replace(R.id.frameLayout, analysis).commitAllowingStateLoss();
                     break;
                 case R.id.profile:
-                    transaction.replace(R.id.frameLayout,profile).commitAllowingStateLoss();
+                    transaction.replace(R.id.frameLayout, profile).commitAllowingStateLoss();
                     break;
             }
             return true;
